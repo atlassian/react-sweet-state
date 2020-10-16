907291995a2cd13651b4329bc543e08621723be5
Alberto Gasparin <albertogasparin@gmail.com>
2020-10-16 16:51:29 +1100

    Ensure state is shallow checked

--- src/middlewares/update.js
+++ src/middlewares/update.js
@@ -1,10 +1,12 @@
+import shallowEqual from '../utils/shallow-equal';
+
 const updateMiddleware = storeState => next => arg => {
   let output;
   const state = storeState.getState();
   const nextState = next(state, arg, out => {
     output = out;
   });
-  if (nextState !== state) {
+  if (!shallowEqual(nextState, state)) {
     storeState.setState(nextState);
   }
   return output;
